40dc78d37c1718a9df82b8f4d620b9a2a2be1487
tabinash123 <tabinash123@gmail.com>
2024-09-15 20:01:56 +0545

    Your 2nd  commit message

--- src/pages/Hompage.js
+++ src/pages/Hompage.js
@@ -1,8 +1,7 @@
 // Homepage.js
 import React from 'react';
-import CTAComponent from './../component/hompage/CTAComponent';
-import DealOfTheDay from './../component/hompage/DealOfTheDay';
-import FAQComponent from './../component/hompage/FAQComponent';
+// import CTAComponent from './../component/hompage/CTAComponent';
+// import FAQComponent from './../component/hompage/FAQComponent';
 import FeatureBar from './../component/hompage/FeatureBar';
 // import GetInTouch from './../component/hompage/GetInTouch';
 import HeroSection from './../component/hompage/HeroSection';
@@ -10,7 +9,7 @@ import IntroductionSection from './../component/hompage/IntroductionSection';
 import ProductHighLight from '../component/hompage/ProductHighLight';
 import Services from './../component/hompage/Services';
 import TestimonialComponent from './../component/hompage/TestimonialComponent';
-import ProductShowcase from '../component/hompage/ProductShowcase';
+// import ProductShowcase from '../component/hompage/ProductShowcase';
 import TrendingProducts from '../component/hompage/TrendingProducts';
 
 const Homepage = () => {
